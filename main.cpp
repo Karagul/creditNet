dbb4668f68b885aca7b4bf8f616c99dd16072859
Filippos Papadopoulos <openrisk@outlook.com>
2019-04-24 10:35:42 +0200

    Placeholder release

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/* (c) 2017-2019 Open Risk (https://www.openriskmanagement.com)
+
+ creditNet is licensed under the Apache 2.0 license a copy of which is included
+ in the source distribution of creditNet. This is notwithstanding any licenses of
+ third-party software included in this distribution. You may not use this file except in
+ compliance with the License.
+
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ either express or implied. See the License for the specific language governing permissions and
+ limitations under the License.
+*/
